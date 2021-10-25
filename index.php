79238309f0e11497d10db8369ab4428e0c35e541
sergicasado <sergicasado1@iesebre.com>
2021-10-25 19:20:45 +0200

    php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+
+echo '<h1>Hola món!</h1>';
