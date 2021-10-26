a83f5707eb63adc0789f0a01fd841e7e1348cb6d
sergicasado <sergicasado1@iesebre.com>
2021-10-26 16:09:58 +0200

    php actualitzat

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<!--Separation of concerns -> Separar "coses diferents"/concerns en diferents fitxers o seccions -> components-->
-<!--MVC -> Model vista controlador -> FC -->
 <?php
 
-echo '<h1>Hola món!</h1>';
-?>
-</body>
+
+require 'app/index.php';
+
+
+require 'resources/views/index.blade.php';
