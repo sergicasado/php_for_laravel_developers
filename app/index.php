fc24195d3d4ac17c841572cc18793f53ed4144b7
sergicasado <sergicasado1@iesebre.com>
2021-10-29 17:02:07 +0200

    php actualitzat

--- app/index.php
+++ app/index.php
@@ -1,27 +1,30 @@
 <?php
 
-class Task{
-    protected $id;
-    protected $title;
-    protected $description;
-    protected $completed;
+require 'app/helpers.php';
+require 'app/Task.php';
 
-    public function __construct($id,$title,$description,$completed)
-    {
-        $this->id = $id;
-        $this->title = $title;
-        $this->description = $description;
-        $this->completed = $completed;
-    }
-}
-
-$task = new Task(1,'comprar pa','a la panaderia',0);
-
-var_dump($task);
+//$task = new Task(1,'comprar pa','a la panaderia',0);
+//var_dump($task);
 
 //  QUERY STRING //var_dump ($_GET['name']); //$name  = 'Sergi';
 //API
 
+$user = 'debian-sys-maint';
+$pass = '0jqqUcbvi2x4JwUb';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=phplaraveldevs',$user,$pass);
+
+}catch (\Exception $e){
+    echo 'Error de connexió a la base de dades';
+}
+
+$statement = $dbh -> prepare('SELECT * FROM tasks;');
+$statement->execute();
+
+
+//Fetch all tasks
+$tasks = $statement->fetchAll(PDO::FETCH_CLASS,'Task');
+
 $greeting = greet();
-//$greeting = 'Hola ' . $_GET['name'] . ' ' . $_GET['name'] . '!';
 
